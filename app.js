77b3f41bed2088314fb650ecb2f638b6d6e38a0c
Anam Ahmed <anam@Anams-MacBook-Air.local>
2015-06-04 23:10:48 +0600

    added resize option, exit working

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var gui = require('nw.gui');
 //console.log(gui.App.argv);
+//video Sizes, maybe someday it will be dynamic
+var sizes = [
+    {width:320,height:240},
+    {width:640,height:480}
+];
 //initiate video
 var video = navigator.webkitGetUserMedia({audio : false, video : true},function(stream){
     var video = document.querySelector('#main');
@@ -9,6 +14,12 @@ var video = navigator.webkitGetUserMedia({audio : false, video : true},function(
     console.log(err);
 });
 document.querySelector('.exiticon').addEventListener('click',function(){
-    console.log('hello');
     gui.App.quit();
-})
+});
+document.querySelector('.sizeicon').addEventListener('click',function(){   document.querySelector('.settings').style.display="block";
+});
+document.querySelector('.ok').addEventListener('click',function(){
+    var newSize = +document.querySelector('#size').value;
+    window.resizeTo(sizes[newSize].width,sizes[newSize].height);
+    document.querySelector('.settings').style.display="none";
+});
